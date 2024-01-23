60c1c3ed51337cbf2a3dcfa2e64886678f7922c1
Maike Vogelbacher <uzuqo@student.kit.edu>
2024-01-23 16:54:58 +0100

    Included gamma function to get correct results for coulomb interaction. Still mistakes, as the ESPs are about as high as those from the MM on the QM zone but should be a fraction of it

--- part2_esp3.py
+++ part2_esp3.py
@@ -3,7 +3,7 @@ import numpy as np
 #file_path = 'test.qxyz'
 file_path = 'qm_dftb_qm.qxyz'
 
-
+# function to detect how many frames and atoms per frame are in the input file
 def find_frames(file_path):
     frames = []
     empty_lines = []
@@ -35,7 +35,7 @@ def find_frames(file_path):
 
     return frames, empty_lines, atom_count1
 
-
+# function to get the coordinates and charges from the input file
 def read_coords_charges(file_path, frames, empty_lines):
     # create empty dictionary 
     dict_frames = {}
@@ -74,6 +74,52 @@ def read_coords_charges(file_path, frames, empty_lines):
 
     return dict_frames
 
+# function for gamma if ESP is calculated between to atoms of the same type
+def g_case_a_eq_b(hubbard1: float, x_range: np.array)->float:
+    # same atoms
+    alpha = 16/5 * hubbard1
+    r = x_range
+    g_result = 1/r - ((1/(48*r)) * (48 + 33 * alpha * r + 9 * alpha**2 * r**2 + alpha**3 * r**3 ) * np.exp(-alpha*r))
+    return g_result
+
+# function for gamma if ESP is calculated between to atoms of different types
+def f_function_gamma(hubbard1: float,hubbard2: float, x_range: np.array)->float:
+    alpha = 16/5 * hubbard1
+    beta = 16/5 * hubbard2
+    r = x_range
+    a_1 = alpha * beta**4
+    a_2 = 2*(alpha**2 - beta**2)**2
+    a_3 = beta**6 - 3 * alpha**2 * beta**4
+    a_4 = r * (alpha**2 -  beta**2)**3
+    f_result = (a_1/a_2) - (a_3/a_4)
+    return f_result
+
+def f_case_a_neq_b(hubbard1: float,hubbard2: float, x_range: np.array)->float:
+    alpha = 16/5 * hubbard1
+    beta = 16/5 * hubbard2
+    r = x_range
+    f_result = 1/r - ( np.exp(-alpha*r) * f_function_gamma(hubbard1, hubbard2, r) + np.exp(-beta*r) * f_function_gamma(hubbard2, hubbard1, r))
+    return f_result
+
+# dictionary to convert numbers to atom types
+dict_atomtypes_reverse = {1:'C', 2:'H', 3:'S'}
+
+# dictionary to define hubbard units
+#unit_conversion_bohr_to_nm = 1/ 0.0529177249
+unit_conversion_bohr_to_nm = 1  # for a.u.
+dict_hubbard = {
+           "C": 0.3647*unit_conversion_bohr_to_nm,
+            "H": 0.4195*unit_conversion_bohr_to_nm,
+            "N": 0.4309*unit_conversion_bohr_to_nm,
+            "O": 0.4954*unit_conversion_bohr_to_nm,
+            "Br": 0.3277*unit_conversion_bohr_to_nm,
+            "Cl": 0.3668*unit_conversion_bohr_to_nm,
+            "F": 0.5584*unit_conversion_bohr_to_nm,
+            "I": 0.2842*unit_conversion_bohr_to_nm,
+            "P": 0.2894*unit_conversion_bohr_to_nm,
+            "S": 0.3288*unit_conversion_bohr_to_nm,
+            }
+
 frames, empty_lines, atom_count = find_frames(file_path)
 #print(frames)
 #print(empty_lines)
@@ -83,7 +129,7 @@ dict_frames = read_coords_charges(file_path, frames, empty_lines)
 #empty dictionary to store ESPs per frame
 dict_esp = {}
 
-print('\nDictionary:')
+#print('\nDictionary:')
 
 # loop through the frames
 for key in dict_frames:
@@ -94,13 +140,16 @@ for key in dict_frames:
     quotient = []
     # empty list to store ESPs of a frame
     esp_values = []
+    print(f'Frame {key}\n')
 
     # loop through the atoms in a frame
     for i in range(0, atom_count):
         # save coordinates of atoms in variable coord_vector if atoms != atom i
         coord_vector_i = dict_frames[key]
         coord_vector_i = np.array([ coord_vector_i[i,1], coord_vector_i[i,2], coord_vector_i[i,3] ])
-        coords.append(coord_vector_i)
+        # convert distance from Angstrom to Bohr
+        coords.append(coord_vector_i * 0.5291772)
+        #print(coords)
 
     # calculate the distance and charge between atom i and all other atoms
     for i in range(0, len(coords)):
@@ -108,7 +157,7 @@ for key in dict_frames:
         ### something's wrong, sum_charge is not about -1
         sum_charge = 0
         
-        print(f'New atom {i}')
+        #print(f'\nNew atom {i}')
         for j in range(0, len(coords)):
             if j != i: ### richtig oder doch andersrum?
                 dist_i = np.linalg.norm(coords[i] - coords[j])
@@ -116,12 +165,28 @@ for key in dict_frames:
                 charge_i = charge_i[j,4]
                 #charge_i = charge_i[i,4]
                 #print('Charge atom i: ', charge_i)
-                # calculate the esp on atom i
-                esp_i += charge_i / dist_i
+                # differentiate between ESP between same or different atom types
+                atom_type_i = dict_frames[key]
+                atom_type_i = int(atom_type_i[i,0])
+                atom_type_j = dict_frames[key]
+                atom_type_j = int(atom_type_j[j,0])
+                if atom_type_i == atom_type_i:
+                    hubbard1  = dict_atomtypes_reverse[atom_type_i]
+                    hubbard1 = dict_hubbard[hubbard1]
+                    gamma_result = g_case_a_eq_b(hubbard1, dist_i)
+                else:
+                    hubbard1 = dict_atomtypes_reverse[atom_type_i]
+                    hubbard1 = dict_hubbard[hubbard1]
+                    hubbard2 = dict_atomtypes_reverse[atom_type_j]
+                    hubbard2 = dict_hubbard[hubbard2]
+                    gamma_result = f_case_a_neq_b(hubbard1, hubbard2, dist_i)
+                #print(gamma_result)
+                # change sign of esp to be compatible with DFTB+ output
+                esp_i += charge_i / gamma_result * (-1) # ESP in a.u.
                 ###
                 sum_charge += charge_i
 
-            print('Sum over charges on atom i: ', sum_charge)
+            #print('Sum over charges on atom i: ', sum_charge)
         esp_values.append(esp_i)
                 
 
@@ -134,6 +199,7 @@ for key in dict_frames:
 #    print(key, dict_esp[key], '\n')
 
 with open('esp_output3', 'w') as file:
+    file.write('QM potential induced on the QM atoms (ESP) in a.u.:\n')
     for key in dict_esp:
         value = dict_esp[key]
         output_string = str(key)
