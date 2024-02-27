d8c96f1e661839c12d8de234a472914141de3a44
maike713 <maike4488@gmx.de>
2024-02-27 13:28:12 +0100

    The sign convention for the partial ESP, so the ESP from the MM on the QM region, is a positive sign for negative charges. However, the sign in the output file qm_dtfb_charges.xvg has the right sign. So the ESP calculated within the QM region already has the right sign and only the sign for the partial ESP needs to be changed. Corrected this in conv_full_esp.py

--- conv_full_esp.py
+++ conv_full_esp.py
@@ -1,14 +1,15 @@
 import numpy as np
 import math
 
-# conversion factor and * (-1) to get right sign convention
+# conversion factor
 # cf = 4 Pi epsilon_0 * a_0/e = e / E_h
-cf = (1.6021766 * 10**(-19)) / (4.359745 * 10**(-18)) * (-1)
+cf = (1.6021766 * 10**(-19)) / (4.359745 * 10**(-18))
 
 input1 = 'qm_dftb_esp.xvg'
 input2 = 'esp_output'
 
-full_esp = (np.loadtxt(input1) + np.loadtxt(input2, skiprows = 1)) *  cf
+# ESP in input1 is with the wrong sign convention, so multiplied with -1
+full_esp = ((np.loadtxt(input1) * (-1)) + np.loadtxt(input2, skiprows = 1)) *  cf
 full_esp[:,0] = (full_esp[:,0] / cf) / 2
 print(full_esp)
 
