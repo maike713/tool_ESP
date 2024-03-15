26d67013049746924d8f0ede444549ce117f2683
maike713 <maike4488@gmx.de>
2024-03-15 20:48:44 +0100

    Linter improvements to the last files, conv_full_esp and plot_esp

--- conv_full_esp.py
+++ conv_full_esp.py
@@ -1,16 +1,15 @@
 import numpy as np
-import math
 
 # conversion factor
-# cf = 4 Pi epsilon_0 * a_0/e = e / E_h
-cf = (1.6021766 * 10**(-19)) / (4.359745 * 10**(-18))
+# CF = 4 Pi epsilon_0 * a_0/e = e / E_h
+CF = (1.6021766 * 10**(-19)) / (4.359745 * 10**(-18))
 
-input1 = 'qm_dftb_esp.xvg'
-input2 = 'esp_output'
+INPUT1 = 'qm_dftb_esp.xvg'
+INPUT2 = 'esp_output'
 
-# ESP in input1 is with the wrong sign convention, so multiplied with -1
-full_esp = ((np.loadtxt(input1) * (-1)) + np.loadtxt(input2, skiprows = 1)) *  cf
-full_esp[:,0] = np.arange(0, len(full_esp[:,0] + 1))    # needed to preserve the time
+# ESP in INPUT1 is with the wrong sign convention, so multiplied with -1
+full_esp = ((np.loadtxt(INPUT1) * (-1)) + np.loadtxt(INPUT2, skiprows = 1)) *  CF
+full_esp[:,0] = np.arange(0, len(full_esp[:,0] + 1))  # needed to preserve the time
 
 np.save('esp_full.npy', full_esp)
 np.savetxt('esp_full.txt', full_esp)
--- plot_esp.py
+++ plot_esp.py
@@ -1,32 +1,34 @@
+"""
+Example: Plot the ESP and its moving average
+"""
 import numpy as np
 import matplotlib.pyplot as plt
-import math
 
 plt.style.use('tableau-colorblind10')
 
-input_file = 'esp_full.txt'
+INPUT_FILE = 'esp_full.txt'
 
-time = np.loadtxt(input_file)[:,0] * 160 / 1000
-esp_22 = np.loadtxt(input_file, usecols = 4)
-esp_2  = np.loadtxt(input_file, usecols = 8)
-esp_27 = np.loadtxt(input_file, usecols = 12)
+time = np.loadtxt(INPUT_FILE)[:, 0] * 160 / 1000
+esp_22 = np.loadtxt(INPUT_FILE, usecols=4)
+esp_2 = np.loadtxt(INPUT_FILE, usecols=8)
+esp_27 = np.loadtxt(INPUT_FILE, usecols=12)
 
-window_size = 50
-kernel = np.ones(window_size) / window_size
-esp_22_ma = np.convolve(esp_22, kernel, mode = 'valid')
-esp_2_ma = np.convolve(esp_2, kernel, mode = 'valid')
-esp_27_ma = np.convolve(esp_27, kernel, mode = 'valid')
+WINDOW_SIZE = 50
+kernel = np.ones(WINDOW_SIZE) / WINDOW_SIZE
+esp_22_ma = np.convolve(esp_22, kernel, mode='valid')
+esp_2_ma = np.convolve(esp_2, kernel, mode='valid')
+esp_27_ma = np.convolve(esp_27, kernel, mode='valid')
 
 
-fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex = True, figsize = (16,8))
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True, figsize=(16, 8))
 
-ax1.plot(time, esp_22, alpha = 0.5, label = 'CYS22')
-ax2.plot(time, esp_2,  alpha = 0.5, label = 'CYS2')
-ax3.plot(time, esp_27, alpha = 0.5, label = 'CYS27')
+ax1.plot(time, esp_22, alpha=0.5, label='CYS22')
+ax2.plot(time, esp_2,  alpha=0.5, label='CYS2')
+ax3.plot(time, esp_27, alpha=0.5, label='CYS27')
 
-ax1.plot(time[window_size-1:], esp_22_ma, label = 'Moving Average')
-ax2.plot(time[window_size-1:], esp_2_ma, label = 'Moving Average')
-ax3.plot(time[window_size-1:], esp_27_ma, label = 'Moving Average')
+ax1.plot(time[WINDOW_SIZE-1:], esp_22_ma, label='Moving Average')
+ax2.plot(time[WINDOW_SIZE-1:], esp_2_ma, label='Moving Average')
+ax3.plot(time[WINDOW_SIZE-1:], esp_27_ma, label='Moving Average')
 
 ax1.set_title('ESP of the S-Atoms of the respective Cystein')
 
@@ -36,8 +38,8 @@ ax3.set_ylabel('ESP in Volt')
 
 ax3.set_xlabel('Time in ps')
 
-ax1.legend(loc = 'upper left')
-ax2.legend(loc = 'upper left')
-ax3.legend(loc = 'upper left')
+ax1.legend(loc='upper left')
+ax2.legend(loc='upper left')
+ax3.legend(loc='upper left')
 
 plt.show()
